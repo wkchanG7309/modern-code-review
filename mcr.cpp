cc6aa00fbe2cb166316ba244a93cd47b9e350bf3
wkchanG7309 <wkchan@cityu.edu.hk>
2020-09-07 21:27:27 +0800

    Update mcr.cpp

--- mcr.cpp
+++ mcr.cpp
@@ -3,11 +3,11 @@ bool isWin(char game[3][3]){
 
 bool win = false; 
 
-if (game[0][0] === game[0][1] && game[0][1] == game[0][2] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true; 
+if (game[0][0] == game[0][1] && game[0][1] == game[0][2] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true; 
 
-if (game[1][0] === game[1][1] && game[1][1] == game[1][2] && (game[1][0] == 'X' || game[1][0] == 'O')) win = true; 
+if (game[1][0] == game[1][1] && game[1][1] == game[1][2] && (game[1][0] == 'X' || game[1][0] == 'O')) win = true; 
 
-if (game[2][0] === game[2][1] || game[2][1] == game[2][2] && (game[2][0] == 'X' || game[2][0] == 'O')) win = true; 
+if (game[2][0] == game[2][1] || game[2][1] == game[2][2] && (game[2][0] == 'X' || game[2][0] == 'O')) win = true; 
 
 // column  2 => row 2
 
